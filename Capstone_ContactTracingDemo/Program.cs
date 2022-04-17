66a8fa1372fe534f19b1bdb4c9a261fa8020a3f2
ajSalvatierra <ajSalvatierra@mcm.edu.ph>
2022-04-17 17:36:24 +0800

    Enter a message <Required>

--- Capstone_ContactTracingDemo/Program.cs
+++ Capstone_ContactTracingDemo/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Capstone_ContactTracingDemo
 {// Not for production
+    //
     static class ProgramParrameters
     {
         public static int ContactTracingLength;
